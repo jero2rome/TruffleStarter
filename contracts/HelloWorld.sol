1804c9b095be0a89142e48bb0f8fa236b88afdfd
Jerome Mohanan <jero2rome@gmail.com>
2021-09-19 21:55:21 +0200

    Comments

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,8 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.5.16;
 
+// This is a demo smart contract
+
+/* multi line
+comment */
+
 contract HelloWorld {
-    string private helloMessage = "Hello world";
+    string private helloMessage = "Hello world"; // this stores my string in a variable
 
     function getHelloMessage() public view returns (string memory) {
         return helloMessage;
